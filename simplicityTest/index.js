2c08526f24a07dd23bad7e67b444cd55e53bddce
SimonBryatov <bsr-007@mail.ru>
2018-05-08 21:36:57 +0400

    hello

--- simplicityTest/index.js
+++ simplicityTest/index.js
@@ -1,9 +1,26 @@
 let lTest = require("./lemanTest");
-let Big = require("big.js");
+Big = require("bignumber.js");
+Big.config({ POW_PRECISION: 0 })
+Big.config({ EXPONENTIAL_AT: 1e+9 })
+
+let random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+};
+
+let getBigRandomString = (it_limit) => {
+    let res = "";
+    res += random(1, 99999999999999);
+    for (var i = 1; i < it_limit; i++) {
+        res += random(0, 99999999999999);
+    }
+    return res
+}
+
+
 
 let preTest = (n, primes) => {
     for (let prime of primes) {
-        if (n == Number(prime)) {
+        if (n.eq(new Big(prime))) {
             console.log("Detected equality to saved prime!")
             return 2
         }
@@ -19,21 +36,27 @@ var fs = require('fs');
 var file = fs.readFileSync("./primes.txt", "utf8");
 let primes = file.split(/[^0-9]/gi).filter(Boolean);
 ////////////////////////
-let n = Big(3213312321321321321312321312321213213123123231).mod(2).valueOf()
-console.log(n)
+// let n = Big(num).mod(2).valueOf()
+
+let found = false;
+while (!found) {
+let num = getBigRandomString(25)
+console.log("Inspecting number " + num)
+//console.log(n)
+n = Big(num)
 ////////////////////////
 let k = 10
 let res = [];
 let flag = 1
-let pre = preTest(num, primes)
+let pre = preTest(n, primes)
 if (pre == 0) {
-    console.log(num + " is definetely NOT a prime number")
+    console.log(num + "\n is definetely NOT a prime number")
 } else if (pre == 2) {
-    console.log(num + " is definetely a prime number")
+    console.log(num + "\n is definetely a prime number")
 } else {
 for (var i = 0; i < k; i++) {
-let localRes = Number(lTest(num, primes))
-if (!(localRes == 1 || localRes == num - 1)) {
+let localRes = lTest(n);
+if (!(localRes.eq(1) || localRes.eq(n.minus(1)))) {
     flag = 0;
     break;
 } else res.push(localRes)
@@ -41,10 +64,11 @@ if (!(localRes == 1 || localRes == num - 1)) {
 
 if (!flag) {
     console.log(num + " is definetely NOT a prime number")
-} else if (res.indexOf(num - 1) >= 0) {
-    let mistake = (1 / (Math.pow(2, k))).toFixed(10)
-    console.log(num + " is a prime number with probability of mistake: " + mistake)
 } else {
-    console.log(num + " is a prime number")
+    let mistake = (1 / (Math.pow(2, k))).toFixed(10)
+    console.log(num + "\n is a prime number with probability of mistake: " + mistake)
+    found = true
+    console.log("Size of number is " + num.toString(2).length + " bits")
+}
 }
 }
--- simplicityTest/lemanTest.js
+++ simplicityTest/lemanTest.js
@@ -1,14 +1,14 @@
-let Big = require("big.js");
+//let Big = require("bignumber.js");
 
 module.exports = (n, primes) => {
     let getRandomInt = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    let a = getRandomInt(2, n - 2);
+    let a = getRandomInt(2, 100);
     console.log("Random number a:", a);
     a = new Big(a);
-    let res = a.pow((n - 1) /2).mod(n).valueOf();
-     console.log("Test result: ", res);
+    let res = a.pow(n.minus(1).div(2), n);
+     console.log("Test result: ", res.toString());
      console.log("==================")
     return res
 }
